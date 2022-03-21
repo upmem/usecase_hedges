b24a1ee1774d5a04ab9b3cc5a3d57e7656201f3d
Dimitri Gerin <dgerin@upmem.com>
2022-03-21 17:22:58 +0100

    buildinstall depth in tmp

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-wget https://www.python.org/ftp/python/2.7/Python-2.7.tgz && tar -xzf Python-2.7.tgz && cd Python-2.7 && CC=/usr/bin/clang ./configure && make &&  sudo make altinstall
-git clone https://github.com/numpy/numpy && cd numpy && git checkout v1.13.3 && git submodule update --init --recursive  && sudo python setup.py install
+cd /tmp && wget https://www.python.org/ftp/python/2.7/Python-2.7.tgz && tar -xzf Python-2.7.tgz && cd Python-2.7 && CC=/usr/bin/clang ./configure && make &&  sudo make altinstall
+cd /tmp && git clone https://github.com/numpy/numpy && cd numpy && git checkout v1.13.3 && git submodule update --init --recursive  && sudo python setup.py install
